6e6a1d58c87313fa4cd7d8277f08513cc7d27b80
NedLandy <matheus.v4200@gmail.com>
2020-05-10 23:13:19 -0300

    correção na geração de arquivos

--- palavras/wordsGen.c
+++ palavras/wordsGen.c
@@ -15,7 +15,6 @@ int main(int argc, char *argv[]){
     //Execução
         srand(time(NULL));//Cria a seed para geração de números aleatórios
         arq = fopen(nomeArq,"w");//abre o arquivo
-        fprintf(arq,"%d\n",qtdPalavras);//salva primeiramente a quantidade de palavras que serão geradas
         for(int i=0;i<qtdPalavras;i++){//repete "numero de palavras" vezes
             for(int j=0;j<tamanhoPalavras;j++){//cria cada letra da palavra
                 palavra[j] = (char) (rand()%26+97);//gera um numero aleatorio entre 0 e 25, e soma a 97, 97 = 'a' em asc
