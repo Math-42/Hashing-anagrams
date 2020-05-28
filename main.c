60e3506f4de2793fa858c8951280e87e832b440c
NedLandy <matheus.v4200@gmail.com>
2020-05-28 15:52:06 -0300

    Comentário completo

--- main.c
+++ main.c
@@ -4,38 +4,41 @@
 
 /*
     A função hash que gera a key indicando onde que cada palavra deve ser salva
-    funciona com base na base numérica combinatorial, que funciona da seguinte forma:
+    funciona utilizando a base numérica combinatorial, da seguinte forma:
 
     Dado uma combinação de n fatores tomados k a k, tem se formada então uma base numérica de 
-    forma que dado qualquer uma das possiveis combinações de forma ordenada ela possui um indice,
-    e esse indice é possivel der ser encontrado a partir da combinação e vice versa.
+    modo qualquer combinação possui um indice numérico e pode ser encontrada com base nesse indice, ou
+    a partir do indice pode se encontrar a combinação.
 
-    Ou seja, as possiveis combinações funciomam como uma base numérica, semelhante a qualquer outra como binário
-    ou decimal.
+    Ou seja, dada uma combinação é possivel encontrar o indice da mesma dentro do universo de todas as
+    combinações possiveis do problema
 
     ex: 
 
     Tomando a=0,b=1,c=2 ... i=8,e utilizando 5 letras de cada vez temos:
 
-    {0} <=> {0,0,0,0,0}
-    {1} <=> {0,0,0,0,1}
-    {2} <=> {0,0,0,0,2}
-    {3} <=> {0,0,0,0,3}
-    {4} <=> {0,0,0,0,4}
-    {5} <=> {0,0,0,0,5}
-    {6} <=> {0,0,0,0,6}
-    {7} <=> {0,0,0,0,7}
-    {8} <=> {0,0,0,0,8}
-    {9} <=> {0,0,0,1,1}
-    {10} <=> {0,0,0,1,2}
-        ....
-    {1285} <=> {7,8,8,8,8}
-    {1286} <=> {8,8,8,8,8}
+    {a,a,a,a,a} => {0,0,0,0,0} <=> {0}
+    {a,a,a,a,b} => {0,0,0,0,1} <=> {1}
+    {a,a,a,a,c} => {0,0,0,0,2} <=> {2}
+    {a,a,a,a,d} => {0,0,0,0,3} <=> {3}
+    {a,a,a,a,e} => {0,0,0,0,4} <=> {4}
+    {a,a,a,a,f} => {0,0,0,0,5} <=> {5}
+    {a,a,a,a,g} => {0,0,0,0,6} <=> {6}
+    {a,a,a,a,h} => {0,0,0,0,7} <=> {7}
+    {a,a,a,a,i} => {0,0,0,0,8} <=> {8}
+    {a,a,a,b,b} => {0,0,0,1,1} <=> {9}
+    {a,a,a,b,c} => {0,0,0,1,2} <=> {10}
+                ...
+    {h,i,i,i,i} => {7,8,8,8,8} <=> {1285}
+    {i,i,i,i,i} => {8,8,8,8,8} <=> {1286}
 
-    note que como anagramas quando são ordenados se tornam iguais dois anagramas mesmo que distintos 
-    retornaram a mesma chave, podendo então ter um hash estático do tamanho exato de possibilidades de anagramas
-    sem causar nenhum conflito indesejável, podendo expandir para qualquer tamanho de string e conjunto de simbolos
-    mantendo complexidade O(c) de insersão sendo c o número de caracteres nas strings.
+    note que anagramas quando são ordenados se tornam  exatamente iguais, logo dois anagramas mesmo que "distintos"
+    retornarão a mesma chave, podendo então ter um hash estático aberto do tamanho exato de possibilidades de anagramas
+    sem causar nenhum conflito indesejável e evitando a alocação de grandes vetores e inserção, podendo expandir para qualquer
+    tamanho de string e conjunto de simbolos (no caso letras) mantendo complexidade de inserção O(c) de insersão sendo c o número 
+    de caracteres nas strings.
+
+    ref: https://en.wikipedia.org/wiki/Combinatorial_number_system
 */
 
 int main(){
